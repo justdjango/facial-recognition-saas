8e450d629192c1adbba69c7c9510c9c193db3da2
mattfreire <freire1996@gmail.com>
2019-05-20 16:47:21 +0200

    Private routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Hoc from "./hoc/hoc";
-
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import LandingPage from "./containers/LandingPage";
 import Demo from "./containers/Demo";
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const authenticated = localStorage.getItem("token") !== null;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authenticated === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
 const BaseRouter = () => (
   <Hoc>
-    <Route exact path="/login/" component={Login} />
-    <Route exact path="/signup/" component={Signup} />
-    <Route exact path="/demo/" component={Demo} />
     <Route exact path="/" component={LandingPage} />
+    <Route path="/login" component={Login} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/demo" component={Demo} />
+    <PrivateRoute path="/account" component={LandingPage} />
   </Hoc>
 );
 
